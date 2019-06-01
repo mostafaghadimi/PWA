2e1c7b37f5f8176a55622313e79a9d19098dee30
Mostafa <mostafa.ghadimi@yahoo.com>
2019-06-01 10:58:08 +0430

    slides and project updated

--- first app/public/service-worker.js
+++ first app/public/service-worker.js
@@ -25,30 +25,7 @@ const FILES_TO_CACHE = [
   '/offline.html',
 ];
 
-// const FILES_TO_CACHE = [
-//   '/',
-//   '/index.html',
-//   '/scripts/app.js',
-//   '/scripts/install.js',
-//   '/scripts/luxon-1.11.4.js',
-//   '/styles/inline.css',
-//   '/images/add.svg',
-//   '/images/clear-day.svg',
-//   '/images/clear-night.svg',
-//   '/images/cloudy.svg',
-//   '/images/fog.svg',
-//   '/images/hail.svg',
-//   '/images/install.svg',
-//   '/images/partly-cloudy-day.svg',
-//   '/images/partly-cloudy-night.svg',
-//   '/images/rain.svg',
-//   '/images/refresh.svg',
-//   '/images/sleet.svg',
-//   '/images/snow.svg',
-//   '/images/thunderstorm.svg',
-//   '/images/tornado.svg',
-//   '/images/wind.svg',
-// ];
+// const FILES_TO1
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
